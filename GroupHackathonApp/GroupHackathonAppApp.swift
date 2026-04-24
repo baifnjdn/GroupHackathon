b80840b4e8a6bebe4c1efa6d28dbeede6e7d4739
Jason <baifnjdn@gmail.com>
2026-04-24 12:58:04 -0400

    Initial Commit

---
+++ GroupHackathonApp/GroupHackathonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupHackathonAppApp.swift
+//  GroupHackathonApp
+//
+//  Created by Jason on 2026-04-24.
+//
+
+import SwiftUI
+
+@main
+struct GroupHackathonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GroupHackathonAppTests/GroupHackathonAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GroupHackathonAppTests.swift
+//  GroupHackathonAppTests
+//
+//  Created by Jason on 2026-04-24.
+//
+
+import Testing
+@testable import GroupHackathonApp
+
+struct GroupHackathonAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
